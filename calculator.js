0689d0d46fb78ca67cde89663530d5d870ee7c6e
Mojeed Adebayo <59479943+greattt33@users.noreply.github.com>
2022-06-24 13:04:00 +0100

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,26 @@
+
+
+    function sum(num1, num2){
+        return num1 + num2
+    }
+    function minus(num1,num2){
+        return console.log(num1 - num2)
+    }
+    function divide(num1,num2){
+        return console.log(num1 / num2)
+    }
+    function multiply(num1,num2){
+        return console.log(num1 * num2)
+    }
+    function modulo(num1,num2){
+        return console.log(num1 % num2)
+    }
+
+    sum(5,4)
+    minus(5,4)
+    divide(5,4)
+    multiply(5,4)
+    modulo(5,4)
+
+
+
